17f72ff2131a7b708f6380a3ff83082e74743930
carlosdealcantara <carlosdealcantara@gmail.com>
2025-05-17 14:08:46 -0300

    Retrocedendo para corrigir

--- equipe.php
+++ equipe.php
@@ -1000,7 +1000,7 @@ include 'includes/header.php';
                                     if (!empty($host['online_description'])) {
                                         echo $host['online_description'];
                                     } else {
-                                        echo !empty($host['description']) ? $host['description'] : '';
+                                        echo '';
                                     }
                                     ?>
                                 </p>
@@ -1011,7 +1011,7 @@ include 'includes/header.php';
                                     if (!empty($host['inperson_description'])) {
                                         echo $host['inperson_description'];
                                     } else {
-                                        echo !empty($host['description']) ? $host['description'] : '';
+                                        echo '';
                                     }
                                     ?>
                                 </p>
@@ -1022,7 +1022,7 @@ include 'includes/header.php';
                                     if (!empty($host['technical_description'])) {
                                         echo $host['technical_description'];
                                     } else {
-                                        echo !empty($host['description']) ? $host['description'] : '';
+                                        echo '';
                                     }
                                     ?>
                                 </p>
