040449bd7fc4e0c06663f215811c0427f489f34b
carlosdealcantara <carlosdealcantara@gmail.com>
2026-05-01 16:31:25 -0300

    cleanup: remove temporary diagnostic and migration scripts

--- check_content.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'config.php';
-$conn = connectDB();
-$stmt = $conn->query("SELECT title, description FROM events LIMIT 3");
-while($row = $stmt->fetch()) {
-    echo "TITULO: " . $row['title'] . " | DESC: " . $row['description'] . "\n";
-}
-?>
