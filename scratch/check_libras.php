edf747604c46b3dd2755417347b908906fc99c8d
carlosdealcantara <carlosdealcantara@gmail.com>
2026-05-02 19:57:40 -0300

    chore: cleanup scratch files

--- scratch/check_libras.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/../config.php';
-$conn = connectDB();
-$stmt = $conn->query("SELECT id, name, flag_code, flag_emoji FROM languages WHERE name LIKE '%Libras%'");
-print_r($stmt->fetchAll());
