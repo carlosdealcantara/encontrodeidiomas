5712e832af59db800f67395f7c9c956f6d9b0212
carlosdealcantara <carlosdealcantara@gmail.com>
2026-05-05 17:31:42 -0300

    feat: aplicado contraste com versão resumida e miniaturizada do selo no Hero

--- index.php
+++ index.php
@@ -56,8 +56,8 @@ include 'includes/header.php';
                                     <i class="fas fa-award"></i>
                                 </div>
                                 <div class="seal-text">
-                                    <span class="seal-eyebrow">Referência Nacional</span>
-                                    <span class="seal-main">1º lugar no país</span>
+                                    <span class="seal-eyebrow">Referência</span>
+                                    <span class="seal-main">Nº 1 do país</span>
                                 </div>
                             </div>
                         </div>
