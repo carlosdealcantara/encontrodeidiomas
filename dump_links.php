ac07e2696bab1d64a91019f47709b0ee0105e524
carlosdealcantara <carlosdealcantara@gmail.com>
2026-05-09 00:44:07 -0300

    temp: add link dumper

---
+++ dump_links.php
@@ -0,0 +1,5 @@
+<?php
+require_once 'config.php';
+$conn = connectDB();
+$links = $conn->query('SELECT id, title, subtitle, badge FROM useful_links')->fetchAll(PDO::FETCH_ASSOC);
+echo json_encode($links, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
