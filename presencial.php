d58b02ecdaf02368175788c94dffed5e971a130c
carlosdealcantara <carlosdealcantara@gmail.com>
2026-05-08 21:50:32 -0300

    fix(seo): localize dynamic meta keywords for presencial page

--- presencial.php
+++ presencial.php
@@ -10,19 +10,19 @@ if (!empty($_GET['cidade'])) {
     $cidade_slug = htmlspecialchars($_GET['cidade']);
     $title = t('presencial.title_city', ['city' => $cidade_slug, 'site_name' => SITE_NAME]);
     $og_description = t('presencial.meta_description_city', ['city' => $cidade_slug]);
-    $meta_keywords = "clube poliglota, praticar idiomas, inglês em {$cidade_slug}, conversação em espanhol, encontro gratuito, {$cidade_slug}, aprender idiomas";
+    $meta_keywords = t('presencial.meta_keywords_city', ['city' => $cidade_slug]);
 } elseif (!empty($_GET['estado'])) {
     $estado_slug = htmlspecialchars($_GET['estado']);
     $title = t('presencial.title_state', ['state' => $estado_slug, 'site_name' => SITE_NAME]);
     $og_description = t('presencial.meta_description_state', ['state' => $estado_slug]);
-    $meta_keywords = "clube poliglota, praticar idiomas, inglês no estado de {$estado_slug}, conversação presencial, encontro de idiomas {$estado_slug}";
+    $meta_keywords = t('presencial.meta_keywords_state', ['state' => $estado_slug]);
 } elseif (!empty($_GET['regiao'])) {
     $regiao_slug = htmlspecialchars($_GET['regiao']);
     $title = t('presencial.title_region', ['region' => $regiao_slug, 'site_name' => SITE_NAME]);
     $og_description = t('presencial.meta_description_region', ['region' => $regiao_slug]);
-    $meta_keywords = "clube poliglota, praticar idiomas, inglês na região {$regiao_slug}, encontro gratuito idiomas {$regiao_slug}";
+    $meta_keywords = t('presencial.meta_keywords_region', ['region' => $regiao_slug]);
 } else {
-    $meta_keywords = "clube poliglota, encontro de idiomas, praticar inglês, espanhol presencial, conversação gratuita";
+    $meta_keywords = t('presencial.meta_keywords_default');
 }
 
 $canonical      = SITE_URL . langUrl('presencial.php');
