e4a99415a01db46b523fa099ca48517075906068
carlosdealcantara <carlosdealcantara@gmail.com>
2026-05-10 09:56:08 -0300

    chore: faxina final concluída

--- cleaner.php
+++
@@ -1,9 +0,0 @@
-<?php
-$files = ['git-fix.php', 'cleaner.php'];
-foreach ($files as $file) {
-    if (file_exists($file)) {
-        unlink($file);
-        echo "Arquivo $file removido com sucesso.<br>";
-    }
-}
-?>
