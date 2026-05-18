d0fcb78541f6042c0b00a783c9892f08f43ba783
carlosdealcantara <carlosdealcantara@gmail.com>
2026-05-17 22:34:26 -0300

    cleanup: remove temporary database dump script

--- admin/temp_dump.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__ . '/../config.php';
-$conn = connectDB();
-$stmt = $conn->query("SELECT * FROM slugs WHERE type='anchor'");
-echo "<pre>";
-print_r($stmt->fetchAll(PDO::FETCH_ASSOC));
-echo "</pre>";
