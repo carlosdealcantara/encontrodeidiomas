a82c2d9bad5672ca3585ca59c2cea229b13858b7
carlosdealcantara <carlosdealcantara@gmail.com>
2026-05-28 11:13:34 -0300

    fix diag

--- admin/check_groups2.php
+++ admin/check_groups2.php
@@ -1,1 +1,10 @@
-﻿<?php require 'config.php'; \ = \->query('SELECT nome, group_id FROM meetup_whatsapp_groups LIMIT 3'); echo json_encode(\->fetchAll(PDO::FETCH_ASSOC), JSON_PRETTY_PRINT); ?>
+<?php
+require '../config.php';
+try {
+    $stmt = $conn->query("SELECT nome, group_id FROM meetup_whatsapp_groups LIMIT 5");
+    $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode($res, JSON_PRETTY_PRINT);
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
+}
+?>
