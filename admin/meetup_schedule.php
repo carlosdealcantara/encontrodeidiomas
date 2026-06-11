a935f046334ad0a0436e649d59023397d990b0af
carlosdealcantara <carlosdealcantara@gmail.com>
2026-06-11 18:23:11 -0300

    fix: Remove non-existent includes/functions.php from meetup_schedule

--- admin/meetup_schedule.php
+++ admin/meetup_schedule.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 require_once '../config.php';
-require_once 'includes/functions.php';
 
 if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== true) {
     header('Location: login.php');
