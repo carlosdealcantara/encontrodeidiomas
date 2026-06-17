2d29b66f0ea879703e2e53a88cab3a90e55d0346
carlosdealcantara <carlosdealcantara@gmail.com>
2026-06-17 00:28:21 -0300

    update julyana

--- admin/debug_streaks.php
+++ admin/debug_streaks.php
@@ -2,8 +2,8 @@
 require_once '../config.php';
 $conn = connectDB();
 
-if (isset($_GET['delete_phantom'])) {
-    $conn->query("DELETE FROM mentoria_desafio_streaks WHERE member_jid LIKE '%@g.us%'");
+if (isset($_GET['update_july'])) {
+    $conn->query("UPDATE mentoria_desafio_streaks SET longest_streak = 4, total_completions = 4 WHERE member_name LIKE '%Julyana%' OR member_name LIKE '%July%'");
 }
 
 $res = $conn->query("SELECT * FROM mentoria_desafio_streaks ORDER BY id DESC")->fetchAll(PDO::FETCH_ASSOC);
