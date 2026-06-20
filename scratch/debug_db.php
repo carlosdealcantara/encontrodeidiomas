27ff10597ff2fae158fbb61168b21e92fa2cf0c6
carlosdealcantara <carlosdealcantara@gmail.com>
2026-06-20 11:12:13 -0300

    update query

--- scratch/debug_db.php
+++ scratch/debug_db.php
@@ -1,5 +1,5 @@
 <?php
 require_once __DIR__ . '/config.php';
 $conn = connectDB();
-$res = $conn->query("SELECT * FROM mentoria_desafio_streaks")->fetchAll(PDO::FETCH_ASSOC);
+$res = $conn->query("SELECT * FROM class_attendances WHERE aula_date='2026-06-18'")->fetchAll(PDO::FETCH_ASSOC);
 echo json_encode($res, JSON_PRETTY_PRINT);
