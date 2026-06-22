9e1a6e1f265334bffbaec230d7adfddca08e963b
carlosdealcantara <carlosdealcantara@gmail.com>
2026-06-21 21:31:59 -0300

    cleanup: remover script temporario de correcao streak Rayza

--- scratch/debug_db.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/config.php';
-$conn = connectDB();
-$res = $conn->query("SELECT * FROM class_attendances WHERE aula_date='2026-06-18'")->fetchAll(PDO::FETCH_ASSOC);
-echo json_encode($res, JSON_PRETTY_PRINT);
--- scratch/update_host_pass.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once dirname(__DIR__) . '/config.php';
-$conn = connectDB();
-$conn->exec("UPDATE settings SET setting_value = '#eivoceai25' WHERE setting_key = 'hosts_app_password'");
-echo "Senha atualizada!\n";
