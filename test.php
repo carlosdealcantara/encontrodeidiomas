1e1fe23ac7f8030b67b7f9af1321327ce6b63dc0
carlosdealcantara <carlosdealcantara@gmail.com>
2026-06-28 18:27:24 -0300

    temp3

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/config.php';
 $conn = connectDB();
-$stmt = $conn->query("SELECT * FROM odysee_publish_queue WHERE language_id = 9 ORDER BY id DESC LIMIT 5");
+$stmt = $conn->query("SELECT * FROM odysee_publish_queue WHERE language_id = 13 ORDER BY id DESC LIMIT 5");
 $rows = $stmt->fetchAll();
 echo json_encode($rows, JSON_PRETTY_PRINT);
