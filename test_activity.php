0cb4f2e6736f766983b92e0c0bbb2eb97eea59ad
carlosdealcantara <carlosdealcantara@gmail.com>
2026-06-30 23:22:05 -0300

    temp update flavia activity

--- test_activity.php
+++ test_activity.php
@@ -1,5 +1,15 @@
 <?php
 require_once "config.php";
 require_once "includes/whatsapp_helper.php";
-$data = fetchBaileysActivity('2026-06-30');
-echo json_encode($data, JSON_PRETTY_PRINT);
+
+$payload = [
+    'apikey' => 'SenhaMeetups2026',
+    'date' => '2026-06-30',
+    'group_jid' => '120363246518434750@g.us',
+    'member_jid' => '277583904125013@lid',
+    'field' => 'images_sent',
+    'value' => 1
+];
+
+$res = sendBaileysRequest('/mentoria-edit-activity', $payload, 'POST');
+echo json_encode($res, JSON_PRETTY_PRINT);
