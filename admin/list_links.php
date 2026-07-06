66da6f87f4c207ecd4263e4a115c62048c8a22bf
carlosdealcantara <carlosdealcantara@gmail.com>
2026-07-06 19:22:01 -0300

    fix config.php path

--- admin/list_links.php
+++ admin/list_links.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'config.php';
+require_once '../config.php';
 $conn = connectDB();
 $stmt = $conn->query("SELECT titulo_final, odysee_url FROM mentoria_odysee_queue WHERE status='done' ORDER BY drive_file_name ASC");
 while($r = $stmt->fetch(PDO::FETCH_ASSOC)) {
