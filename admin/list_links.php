da3c9145cd0f3d1b8faad7fc9ba6f928189fb0b6
carlosdealcantara <carlosdealcantara@gmail.com>
2026-07-06 19:21:21 -0300

    add temp script to list videos

---
+++ admin/list_links.php
@@ -0,0 +1,8 @@
+<?php
+require_once 'config.php';
+$conn = connectDB();
+$stmt = $conn->query("SELECT titulo_final, odysee_url FROM mentoria_odysee_queue WHERE status='done' ORDER BY drive_file_name ASC");
+while($r = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    echo $r['titulo_final'] . " - " . $r['odysee_url'] . "<br>\n";
+}
+?>
