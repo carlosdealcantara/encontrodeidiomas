1b5fe504f83cfc2a115b34d0d418a8b36e4404f2
carlosdealcantara <carlosdealcantara@gmail.com>
2026-08-02 19:30:36 -0300

    tmp: check meetings

--- scratch/check_logs.php
+++ scratch/check_logs.php
@@ -1,5 +1,9 @@
 <?php
 require_once dirname(__DIR__) . '/config.php';
 $conn = connectDB();
-$stmt = $conn->query("SHOW CREATE TABLE meetup_whatsapp_logs");
-print_r($stmt->fetch());
+$hoje = new DateTime();
+$diaDaSemanaAtual = (int)$hoje->format('N');
+echo "Dia: $diaDaSemanaAtual\n";
+$stmtMeetings = $conn->prepare("SELECT * FROM meetings WHERE active = 1 AND day_of_week = ?");
+$stmtMeetings->execute([$diaDaSemanaAtual]);
+print_r($stmtMeetings->fetchAll());
