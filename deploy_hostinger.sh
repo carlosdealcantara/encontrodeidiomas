5ab55248ccae47c4bd5210b01d4f59c4ed30af68
carlosdealcantara <carlosdealcantara@gmail.com>
2026-08-12 22:43:49 -0300

    fix: update SSH user in deploy_hostinger.sh based on Hostinger panel

--- deploy_hostinger.sh
+++ deploy_hostinger.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sshpass -p '#Nadier38' ssh -o StrictHostKeyChecking=no -p 65002 u879045076_carlos@77.37.127.146 'cd domains/viaei.com/public_html/dev && git pull origin dev'
+sshpass -p '#Nadier38' ssh -o StrictHostKeyChecking=no -p 65002 u879045076@77.37.127.146 'cd domains/viaei.com/public_html/dev && git pull origin dev'
