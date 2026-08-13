98f325b0d00f6e2c3b2c10a9d69d414317288f65
carlosdealcantara <carlosdealcantara@gmail.com>
2026-08-12 22:17:24 -0300

    fix: update DB credentials to new viaei.com database

--- start_mentoria.sh
+++ start_mentoria.sh
@@ -6,8 +6,8 @@ sudo docker run -d --name mentoria-worker \
   --add-host=host.docker.internal:host-gateway \
   -v /home/ubuntu/encontrodeidiomas/google_service_account.json:/app/google_service_account.json \
   -e DB_HOST=77.37.127.146 \
-  -e DB_NAME=u879045076_central \
-  -e DB_USER=u879045076_carlos \
+  -e DB_NAME=u879045076_ei \
+  -e DB_USER=u879045076_ei \
   -e DB_PASS="#Nadier38" \
   -e DRIVE_MENTORIA_FOLDER_ID=1-iW8FuL4UhyfjRQyZ1zdtCiAVVtcLLAU \
   -e DRIVE_MENTORIA_ARCHIVE_FOLDER_ID=11mn7mlIJCCsnCwLP_Ucbmj56fUevjnnj \
