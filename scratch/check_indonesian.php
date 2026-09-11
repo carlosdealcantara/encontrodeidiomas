8271e0bda61c90ecaa70416ccf0663201334de0d
carlosdealcantara <carlosdealcantara@gmail.com>
2026-09-11 14:51:00 -0300

    chore: reverte adicao de endpoint inutil e remove arquivos temporarios

--- scratch/check_indonesian.php
+++
@@ -1,7 +0,0 @@
-﻿<?php
-require_once '../config.php';
-$conn = connectDB();
-$groups = $conn->query("SELECT nome FROM meetup_whatsapp_groups WHERE nome LIKE '%Bahasa%' AND ativo = 1")->fetchAll(PDO::FETCH_ASSOC);
-foreach($groups as $g) {
-    echo $g['nome'] . "\n";
-}
