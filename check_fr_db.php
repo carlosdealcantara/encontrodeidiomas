5d2da877366659e3b5b6bad05d2a1aa6bc510f5a
carlosdealcantara <carlosdealcantara@gmail.com>
2026-09-11 18:37:50 -0300

    temp: diag script

---
+++ check_fr_db.php
@@ -0,0 +1,5 @@
+﻿<?php
+require_once 'config.php';
+$conn = connectDB();
+$stmt = $conn->query("SELECT group_id, nome, ativo, welcome_enabled, lang_code FROM meetup_whatsapp_groups WHERE lang_code = 'fr'");
+echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC), JSON_PRETTY_PRINT);
