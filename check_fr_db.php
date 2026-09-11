32b54b8ced6e59c7199f482a8b8cf2b15385c449
carlosdealcantara <carlosdealcantara@gmail.com>
2026-09-11 18:40:47 -0300

    sync: run community-config sync to Baileys

--- check_fr_db.php
+++ check_fr_db.php
@@ -1,5 +1,26 @@
 <?php
 require_once 'config.php';
+require_once 'includes/whatsapp_helper.php';
+
 $conn = connectDB();
-$stmt = $conn->query("SELECT group_id, nome, ativo, welcome_enabled, lang_code FROM meetup_whatsapp_groups WHERE group_id = '120363164732845564@g.us'");
-echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC), JSON_PRETTY_PRINT);
+$config = getCommunityConfig();
+$stmtGlob = $conn->query("SELECT group_id as jid, nome, welcome_enabled FROM meetup_whatsapp_groups WHERE comunidade = 'global' AND ativo = 1");
+$globalGroups = $stmtGlob->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($globalGroups as $gg) {
+    $key = 'global_' . preg_replace('/[^a-z0-9]/', '', strtolower($gg['nome']));
+    $config['groups'][$key] = [
+        'jid' => $gg['jid'],
+        'name' => $gg['nome'],
+        'is_community_group' => true,
+        'ranking_enabled' => true,
+        'welcome_enabled' => (bool)$gg['welcome_enabled']
+    ];
+}
+
+$res = sendBaileysRequest('/community-config', $config, 'POST');
+echo json_encode([
+    'sent_groups' => count($config['groups']),
+    'baileys_res' => $res,
+    'french_in_groups' => isset($config['groups']['global_eifranaisfrench'])
+]);
