994b5d9b1617af8545ee3a04f788abb90a325bbb
carlosdealcantara <carlosdealcantara@gmail.com>
2026-09-24 01:16:37 -0300

    debug: add php opening tag

--- scratch_check.php
+++ scratch_check.php
@@ -1,3 +1,6 @@
+<?php
+require_once __DIR__ . '/config.php';
+$conn = connectDB();
 require_once __DIR__ . '/includes/whatsapp_helper.php';
 
 $ontem = (new DateTime())->modify('-1 day')->format('Y-m-d');
